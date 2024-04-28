2000aa4005965b6aa5a05a2e630d1bc4adeda528
gordon2022 <11338045+gordonhsu2022@user.noreply.gitee.com>
2024-04-28 18:55:11 +0800

    "first commit"

---
+++ src/main/java/com/gordon/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.gordon.git;
+
+/**
+ * @author Gordon
+ * @create 2024-04-28 18:47
+ * @description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello git");
+    }
+}
