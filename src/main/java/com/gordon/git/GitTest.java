25c36960c88bc7bd9ea67692b3bf9956dce7d3a5
gordon2022 <11338045+gordonhsu2022@user.noreply.gitee.com>
2024-04-28 20:01:04 +0800

    "commit"

--- src/main/java/com/gordon/git/GitTest.java
+++ src/main/java/com/gordon/git/GitTest.java
@@ -8,11 +8,11 @@ package com.gordon.git;
 public class GitTest {
     public static void main(String[] args) {
 
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("hot-fix test");
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("hot-fix test!");
 
     }
 }
